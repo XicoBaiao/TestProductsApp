a2511fb95adeec7fc448e013f60dfe6b2b17cad2
Francisco Baião <fbaiao@deloitte.pt>
2024-12-08 12:22:46 +0000

    Initial commit

---
+++ TestProductsApp/TestProductsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProductsAppApp.swift
+//  TestProductsApp
+//
+//  Created by Francisco Baião on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestProductsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TestProductsAppTests/TestProductsAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TestProductsAppTests.swift
+//  TestProductsAppTests
+//
+//  Created by Francisco Baião on 08/12/2024.
+//
+
+import Testing
+@testable import TestProductsApp
+
+struct TestProductsAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
